3e441b53df63d13b83ab82f64f5eb4872e2876d8
LeeQianXi <qianxilee232@163.com>
2025-11-28 04:33:14 +0800

    Init Branch

---
+++ src/main/java/com/phasetranscrystal/brealib/BreaLib.java
@@ -0,0 +1,27 @@
+package com.phasetranscrystal.brealib;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+import lombok.Getter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Main mod class.
+ * <p>
+ * An example for blocks is in the `blocks` package of this mod.
+ */
+@Mod(BreaLib.MOD_ID)
+public class BreaLib {
+
+    public static final String MOD_ID = "brealib";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+    public static final String NAME = "Brea";
+
+    @Getter
+    private static ModContainer modContainer;
+    @Getter
+    private static IEventBus modEventBus;
+}

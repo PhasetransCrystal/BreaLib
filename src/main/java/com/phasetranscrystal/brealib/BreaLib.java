7306cd8583e16b5c1061d648e56ffc538d5ef00b
LeeQianXi <qianxilee232@163.com>
2025-11-28 09:25:41 +0800

    导入ModernUI-MC

--- src/main/java/com/phasetranscrystal/brealib/BreaLib.java
+++ src/main/java/com/phasetranscrystal/brealib/BreaLib.java
@@ -1,10 +1,7 @@
 package com.phasetranscrystal.brealib;
 
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 
-import lombok.Getter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -19,9 +16,4 @@ public class BreaLib {
     public static final String MOD_ID = "brealib";
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
     public static final String NAME = "Brea";
-
-    @Getter
-    private static ModContainer modContainer;
-    @Getter
-    private static IEventBus modEventBus;
 }
